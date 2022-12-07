da7f9b3478862fdfa4ca9cc3cfa52c0fc6e8a6e8
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 10:52:24 +0000

    need to restructure main App to fix dumb hook issue

--- react/src/src/App.tsx
+++ react/src/src/App.tsx
@@ -11,17 +11,20 @@ const MenuItem = (params: {className: string; onClick: MouseEventHandler<HTMLLIE
 	);
       },
       App = () => {
-	const [selected, setSelected] = useState(-1),
-	      [contents, setContents] = useState((<div>Please select a page to start</div>));
+	const [selected, setSelected] = useState(-1);
 	return (
 		<div>
 			<ul id="tabs">
-				<MenuItem title="Page 1" className={selected === 0 ? "tab_selected" : ""} onClick={() => {setSelected(0); setContents((<Checkbox />))}} />
-				<MenuItem title="Page 2" className={selected === 1 ? "tab_selected" : ""} onClick={() => {setSelected(1); setContents((<Radio />))}} />
-				<MenuItem title="Page 3" className={selected === 2 ? "tab_selected" : ""} onClick={() => {setSelected(2); setContents((<SortWord />))}} />
-				<MenuItem title="Page 4" className={selected === 3 ? "tab_selected" : ""} onClick={() => {setSelected(3); setContents((<SortHash />))}} />
+				<MenuItem title="Page 1" className={selected === 0 ? "tab_selected" : ""} onClick={() => setSelected(0)} />
+				<MenuItem title="Page 2" className={selected === 1 ? "tab_selected" : ""} onClick={() => setSelected(1)} />
+				<MenuItem title="Page 3" className={selected === 2 ? "tab_selected" : ""} onClick={() => setSelected(2)} />
+				<MenuItem title="Page 4" className={selected === 3 ? "tab_selected" : ""} onClick={() => setSelected(3)} />
 			</ul>
-			{contents}
+			<div className={selected !== -1 ? "hidden" : ""}>Please select a page to start</div>
+			<Checkbox hidden={selected !== 0} />
+			<Radio hidden={selected !== 1} />
+			<SortWord hidden={selected !== 2} />
+			<SortHash hidden={selected !== 3} />
 		</div>
 	);
       };
