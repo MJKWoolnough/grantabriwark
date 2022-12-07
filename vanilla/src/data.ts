59c4f5fd60db75703268f140217edf97409f5fa3
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 11:04:00 +0000

    added data for vanilla example

---
+++ vanilla/src/data.ts
@@ -0,0 +1,14 @@
+export default [
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"Nulla pellentesque mi vitae viverra venenatis.",
+	"Morbi condimentum nisi ut magna rhoncus vulputate.",
+	"Etiam luctus nunc ac enim facilisis tristique.",
+	"Quisque ut libero id quam tincidunt aliquam.",
+	"Etiam sit amet augue non lectus sagittis lacinia ac quis.",
+	"Pellentesque quis purus pharetra, pellentesque tortor quis, scelerisque sapien.",
+	"Proin non mauris sit.",
+	"Donec sed quam tincidunt, eleifend massa nec, convallis nisi.",
+	"Duis posuere nunc molestie velit fringilla, id blandit erat varius.",
+	"Phasellus euismod nibh dictum libero consequat mattis.",
+	"Proin ac nulla quis diam vestibulum sollicitudin eu eget odio."
+] as string[];
