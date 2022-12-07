d79fe2adb0368822a2ac3cc0ba12f514af7fbe4e
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 09:50:41 +0000

    added 'radio' list display to select single line

--- radio.ts
+++ radio.ts
@@ -1,3 +1,16 @@
-import {div} from './lib/html.js';
+import {div, input, li, ul} from './lib/html.js';
+import {IntSetting} from './lib/settings.js';
+import data from './data.js';
+import {labels} from './shared.js';
 
-export default div("Page 2");
+const radio = new IntSetting("grantabriwark_radio", -1);
+
+export default div([
+	"Please select the line you like the best: ",
+	ul({"id": "radioWords"}, data.map((line, n) => li(labels(input({"type": "radio", "name": "grantabriwark_radio", "checked": radio.value === n, "onclick": function(this: HTMLInputElement) {
+		if (this.checked) {
+			radio.set(n);
+		}
+	}}), line))))
+]);
+
