852330ee3e8ec754d9d058f84eaf2e1b9e58469c
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 09:48:58 +0000

    added 'checkbox' list display to select multiple lines

--- checkbox.ts
+++ checkbox.ts
@@ -1,3 +1,15 @@
-import {div} from './lib/html.js';
+import {div, input, li, ul} from './lib/html.js';
+import data from './data.js';
+import {labels} from './shared.js';
+import {JSONSetting} from './lib/settings.js';
 
-export default div("Page 1");
+const checked = new JSONSetting("grantabriwark_checked", data.map(() => false), (v: any): v is boolean[] => v instanceof Array && v.every(b => typeof b === "boolean"));
+
+export default div([
+	"Please check the lines you like: ",
+	ul({"id": "checkedWords"}, data.map((line, n) => li(labels(input({"type": "checkbox", "checked": checked.value[n], "onchange": function(this: HTMLInputElement) {
+		checked.value[n] = this.checked;
+		checked.set(checked.value);
+	}}), line))))
+]);
+
