19a3f88d81c564698f930d785d92ada149ab6229
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 11:07:46 +0000

    added vanilla version of checkbox

---
+++ vanilla/src/checkbox.ts
@@ -0,0 +1,27 @@
+import data from './data.js';
+
+const checked = JSON.parse(window.localStorage.getItem("grantabriwark_checked") ?? "[]") as boolean[],
+      div = document.createElement("div"),
+      ul = document.createElement("ul");
+
+ul.setAttribute("id", "checkedWords");
+
+for (let i = 0; i < data.length; i++) {
+	const n = i,
+	      li = ul.appendChild(document.createElement("li")),
+	      input = li.appendChild(document.createElement("input")),
+	      label = li.appendChild(document.createElement("label"));
+	input.addEventListener("click", () => {
+		checked[n] = input.checked;
+		window.localStorage.setItem("grantabriwark_checked", JSON.stringify(checked));
+	});
+	input.setAttribute("type", "checkbox");
+	input.toggleAttribute("checked", !!checked[n]);
+	input.setAttribute("id", "checked_" + n);
+	label.setAttribute("for", "checked_" + n);
+	label.textContent = data[n];
+}
+
+div.append("Please check the lines you like: ", ul);
+
+export default div;
