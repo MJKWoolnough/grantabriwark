f80737bc9d071c71fa48d87f64d757ba64f12000
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 10:15:49 +0000

    added quick build file

---
+++ jslib/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+(
+	cd src;
+	head -n4 index.html;
+	echo -n "		<script type=\"module\">"
+	jspacker -i /src.js -n | terser -m --module --compress pure_getters,passes=3 --ecma 6 | tr -d '\n';
+	echo "</script>";
+	tail -n4 index.html;
+) > index.html
