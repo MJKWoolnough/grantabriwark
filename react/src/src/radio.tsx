a0851e83867529f550d63d895e91ed6b37633a63
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 10:37:43 +0000

    added React version of radio list

--- react/src/src/radio.tsx
+++ react/src/src/radio.tsx
@@ -1,1 +1,23 @@
-export default () => (<div>Page 2</div>);
+import {useId} from 'react';
+import data from './data';
+
+let radio = JSON.parse(window.localStorage.getItem("grantabriwark_radio") ?? "-1");
+
+const RadioLine = (params: any) => {
+	const id = useId();
+	return (
+		<li><input type="radio" name="grantabriwark_radio" defaultChecked={radio === params.num} id={id} onClick={() => {radio = params.num; window.localStorage.setItem("grantabriwark_radio", radio)}} /><label htmlFor={id}>{params.line}</label></li>
+	);
+      },
+      Radio = (params: any) => {
+	return (
+		<div className={params.hidden ? "hidden" : ""}>
+			Please check the line you like the best:
+			<ul id="radioWords">
+				{data.map((line, n) => (<RadioLine key={"radio_" + n} num={n} line={line} />))}
+			</ul>
+		</div>
+	);
+      };
+
+export default Radio;
