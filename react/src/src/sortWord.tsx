e4f9fdec2fbfd504962c0c78bbf71b34209f14a4
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 10:53:18 +0000

    added React version of sortWord

--- react/src/src/sortWord.tsx
+++ react/src/src/sortWord.tsx
@@ -1,1 +1,33 @@
-export default () => (<div>Page 3</div>);
+import {useState} from 'react';
+import data from './data';
+
+const split = data.map((l, id) => ({"words": l.split(" "), id})),
+      maxWords = split.reduce((c, w) => Math.max(c, w.words.length), 0),
+      stringSort = new Intl.Collator().compare,
+      Row = (params: any) => (<tr>
+	{params.words.map((word: string, n: number) => (<td key={n}>{word}</td>))}
+	{Array.from({"length": maxWords - params.words.length}, () => (<td>-</td>))}
+      </tr>),
+      SortWord = (params: {hidden: boolean}) => {
+	const [sortNum, setSortNum] = useState(-1),
+	      [reverse, setReverse] = useState(false);
+	return (
+		<table id="sortWords" className={params.hidden ? "hidden" : ""}>
+			<thead>
+				<tr>{Array.from({"length": maxWords}, (_, n) => (<th className={`${sortNum === n ? "col_selected" : ""} ${reverse ? "reverse" : ""}`} key={n} onClick={() => {
+					if (sortNum === n) {
+						setReverse(!reverse);
+					} else {
+						setSortNum(n);
+						setReverse(false);
+					}
+				}}>Word {n+1}</th>))}</tr>
+			</thead>
+			<tbody>
+				{split.slice().sort(sortNum === -1 ? () => 0 : (a, b) => stringSort(a.words[sortNum] ?? "", b.words[sortNum] ?? "") * (reverse ? -1 : 1)).map(({words, id}) => (<Row key={id} words={words} />))}
+			</tbody>
+		</table>
+	);
+      };
+
+export default SortWord;
