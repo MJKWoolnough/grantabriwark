3237971b4fc0f7c3897c10ddf1ced3934e13b0da
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 10:03:58 +0000

    added simple list styling

--- checkbox.ts
+++ checkbox.ts
@@ -1,9 +1,15 @@
+import {add} from './lib/css.js';
 import {div, input, li, ul} from './lib/html.js';
 import data from './data.js';
 import {labels} from './shared.js';
 import {JSONSetting} from './lib/settings.js';
 
 const checked = new JSONSetting("grantabriwark_checked", data.map(() => false), (v: any): v is boolean[] => v instanceof Array && v.every(b => typeof b === "boolean"));
+
+add("#checkedWords", {
+	"padding": 0,
+	"list-style": "none"
+});
 
 export default div([
 	"Please check the lines you like: ",
