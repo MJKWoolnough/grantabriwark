386d18f612f6e36d06f16ed94c432ec5f690c879
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-07 10:04:44 +0000

    added simple list styling

--- radio.ts
+++ radio.ts
@@ -1,9 +1,15 @@
+import {add} from './lib/css.js';
 import {div, input, li, ul} from './lib/html.js';
 import {IntSetting} from './lib/settings.js';
 import data from './data.js';
 import {labels} from './shared.js';
 
 const radio = new IntSetting("grantabriwark_radio", -1);
+
+add("#radioWords", {
+	"padding": 0,
+	"list-style": "none"
+});
 
 export default div([
 	"Please select the line you like the best: ",
