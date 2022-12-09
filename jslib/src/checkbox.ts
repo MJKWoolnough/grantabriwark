9d51b973172b0ee3476b22b6c0d11d224bd4fd55
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-09 18:08:25 +0000

    correct import order

--- jslib/src/checkbox.ts
+++ jslib/src/checkbox.ts
@@ -1,8 +1,8 @@
 import {add} from './lib/css.js';
 import {div, input, li, ul} from './lib/html.js';
+import {JSONSetting} from './lib/settings.js';
 import data from './data.js';
 import {labels} from './shared.js';
-import {JSONSetting} from './lib/settings.js';
 
 const checked = new JSONSetting("grantabriwark_checked", data.map(() => false), (v: any): v is boolean[] => v instanceof Array && v.every(b => typeof b === "boolean"));
 
