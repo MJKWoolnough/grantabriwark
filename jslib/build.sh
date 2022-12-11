b952944bed31c998e477a59bab21569ea52e4736
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-11 07:49:30 +0000

    added compressed output

--- jslib/build.sh
+++ jslib/build.sh
@@ -7,4 +7,5 @@
 	jspacker -i /src.js -n | terser -m --module --compress pure_getters,passes=3 --ecma 6 | tr -d '\n';
 	echo "</script>";
 	tail -n3 index.html;
-) > index.html
+) > index.html;
+zopfli -m index.html;
