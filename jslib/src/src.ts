5e3fec615a447d3509d2265a017dd3ad76f5fb01
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-11 07:46:42 +0000

    corrected import order

--- jslib/src/src.ts
+++ jslib/src/src.ts
@@ -4,8 +4,8 @@ import {div, li, ul} from './lib/html.js';
 import pageLoad from './lib/load.js';
 import checkbox from './checkbox.js';
 import radio from './radio.js';
-import sortWord from './sortWord.js';
 import sortHash from './sortHash.js';
+import sortWord from './sortWord.js';
 
 pageLoad.then(() => {
 	const content = div("Please select a page to start");
