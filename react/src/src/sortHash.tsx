2819b7bc1d05a1cefec533a523517f19cfd529a9
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-14 15:07:05 +0000

    corrected spacing

--- react/src/src/sortHash.tsx
+++ react/src/src/sortHash.tsx
@@ -5,7 +5,7 @@ const hasSubtle = !!window.crypto.subtle,
       hashString = hasSubtle ? (str: string) => window.crypto.subtle.digest("SHA-1", new TextEncoder().encode(str)).then(d => {
 	const data = new Uint8Array(d);
 	let str = "";
-        for (const a of Array.from(data)) {
+	for (const a of Array.from(data)) {
 		str += ("0" + a.toString(16)).slice(-2);
 	}
 	return str;
