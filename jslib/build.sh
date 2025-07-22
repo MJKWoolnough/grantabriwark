ee22fdb4e147b2e3009cf1259d7343fdb3b104db
Michael Woolnough <michael.woolnough@gmail.com>
2025-07-22 06:07:55 +0100

    re-formatted

--- jslib/build.sh
+++ jslib/build.sh
@@ -3,7 +3,7 @@
 (
 	cd src;
 	head -n4 index.html;
-	echo -n "		<script type=\"module\">"
+	echo -n "		<script type=\"module\">";
 	jspacker -i /src.js -n | terser -m --module --compress pure_getters,passes=3 --ecma 6 | tr -d '\n';
 	echo "</script>";
 	tail -n3 index.html;
