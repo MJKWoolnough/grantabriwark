1c96b306031390951c6ed6b061e7584935f84a37
Michael Woolnough <michael.woolnough@gmail.com>
2025-07-22 06:07:44 +0100

    re-formatted

--- react/build.sh
+++ react/build.sh
@@ -2,10 +2,10 @@
 
 (
 	head -n5 src/public/index.html;
-	echo -n "		<script type=\"module\">"
+	echo -n "		<script type=\"module\">";
 	terser -m --module --compress pure_getters,passes=3 --ecma 6 src/build/static/js/main*.js | tr -d '\n';
 	echo "</script>";
-	echo -n "		<style type=\"text/css\">"
+	echo -n "		<style type=\"text/css\">";
 	cat src/build/static/css/main*.css;
 	echo "</style>";
 	tail -n6 src/public/index.html;
